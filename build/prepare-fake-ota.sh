b2cd777ef6a338d1bd167bc1f833652c73ba6d57
Alexander Martinz <amartinz@shiftphones.com>
2022-01-19 16:21:07 +0100

    axolotl: use focal rootfs
    Change-Id: If623b62c9772ecccb5b66772e369e3168b91eafd
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- build/prepare-fake-ota.sh
+++ build/prepare-fake-ota.sh
@@ -2,9 +2,9 @@
 
 # Fetches android9 rootfs and generic system image to prepare flashable image from CI-built device tarball
 URL='https://system-image.ubports.com'
-ROOTFS_URL='https://ci.ubports.com/job/xenial-hybris-android9-rootfs-armhf/lastSuccessfulBuild/artifact/ubuntu-touch-android9-armhf.tar.gz'
+ROOTFS_URL='https://ci.ubports.com/job/focal-hybris-android9-rootfs-armhf/lastSuccessfulBuild/artifact/ubuntu-touch-android9plus-armhf.tar.gz'
 DEVICE_GENERIC_URL='https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_arm64/job/halium-10.0-arm32/lastSuccessfulBuild/artifact/halium_halium_arm.tar.xz'
-OTA_CHANNEL='16.04/arm64/android9/devel'
+OTA_CHANNEL='20.04/arm64/android9/devel'
 
 DEVICE_TARBALL="$1"
 OUTPUT="$2"
